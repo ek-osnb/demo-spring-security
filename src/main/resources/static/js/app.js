c83754e1bf6913da82200f503d52f074b091e166
osman-butt <butt.osman@gmail.com>
2026-01-12 19:24:11 +0100

    added docs

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,18 @@
+/*
+* This serves as the entry point for the frontend application.
+*
+* This file is referenced in every HTML page.
+* It initializes page-specific logic based on the current URL path.
+*
+* It imports and calls initialization functions from page modules:
+* - initHomePage for index.html
+* - initLoginPage for login.html
+* - initProtectedPage for protected.html
+*
+* Each page module sets up event listeners and UI logic specific to that page.
+*/
+
+
 import {initLoginPage} from "./pages/login.js";
 import {initProtectedPage} from "./pages/protected.js";
 import {initHomePage} from "./pages/home.js";
